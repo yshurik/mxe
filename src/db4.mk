7c7c540d173276eef969defd00a53f37beadb737
yshurik <yshurik@gmail.com>
2018-09-13 00:47:16 +0200

    Add db4

---
+++ src/db4.mk
@@ -0,0 +1,28 @@
+# This file is part of MXE. See LICENSE.md for licensing information.
+
+PKG             := db
+$(PKG)_IGNORE   :=
+$(PKG)_VERSION  := 4.8.30
+$(PKG)_CHECKSUM := ab36c170dda5b2ceaad3915ced96e41c6b7e493c
+$(PKG)_SUBDIR   := db-$($(PKG)_VERSION)
+$(PKG)_FILE     := db-$($(PKG)_VERSION).tar.gz
+$(PKG)_URL      := http://download.oracle.com/berkeley-db/$($(PKG)_FILE)
+$(PKG)_DEPS     := cc
+
+define $(PKG)_UPDATE
+    $(WGET) -q -O- 'https://www.oracle.com/technetwork/database/database-technologies/berkeleydb/downloads/index.html' | \
+    $(SED) -n 's,.*/db-\([0-9\.]\+\)\.tar.gz.*,\1,p' | \
+    head -1
+endef
+
+define $(PKG)_BUILD
+    cd '$(1)/build_unix' && ../dist/configure \
+        $(MXE_CONFIGURE_OPTS) \
+        --enable-mingw \
+        --enable-cxx \
+        --enable-cryptography \
+        --disable-replication
+
+    $(MAKE) -C '$(1)/build_unix' -j '$(JOBS)'
+    $(MAKE) -C '$(1)/build_unix' -j 1 install $(MXE_DISABLE_DOCS)
+endef
