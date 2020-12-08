d33ab76e31a191c5657ee21f9e51f0b0983bb58c
yshurik <yshurik@gmail.com>
2020-12-08 13:26:36 +0100

    fix merge, 3

--- src/gpgme.mk
+++ src/gpgme.mk
@@ -4,13 +4,8 @@ PKG             := gpgme
 $(PKG)_WEBSITE  := https://www.gnupg.org/related_software/gpgme/
 $(PKG)_DESCR    := gpgme
 $(PKG)_IGNORE   :=
-<<<<<<< HEAD
-$(PKG)_VERSION  := 1.11.1
-$(PKG)_CHECKSUM := 2d1b111774d2e3dd26dcd7c251819ce4ef774ec5e566251eb9308fa7542fbd6f
-=======
 $(PKG)_VERSION  := 1.14.0
 $(PKG)_CHECKSUM := cef1f710a6b0d28f5b44242713ad373702d1466dcbe512eb4e754d7f35cd4307
->>>>>>> 2020-12-08
 $(PKG)_SUBDIR   := gpgme-$($(PKG)_VERSION)
 $(PKG)_FILE     := gpgme-$($(PKG)_VERSION).tar.bz2
 $(PKG)_URL      := https://gnupg.org/ftp/gcrypt/gpgme/$($(PKG)_FILE)
@@ -24,11 +19,7 @@ define $(PKG)_UPDATE
 endef
 
 define $(PKG)_BUILD
-<<<<<<< HEAD
-    cd '$(1)' && GPG_ERROR_CONFIG=$(PREFIX)/bin/$(TARGET)-gpg-error-config ./configure \
-=======
     cd '$(1)' && GPG_ERROR_CONFIG=$(PREFIX)/bin/$(TARGET)-gpg-error-config LIBASSUAN_CONFIG='$(PREFIX)/bin/$(TARGET)-libassuan-config' ./configure \
->>>>>>> 2020-12-08
         $(MXE_CONFIGURE_OPTS) \
         --disable-nls \
         --disable-languages
